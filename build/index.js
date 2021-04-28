6ab536f4e633d766d8f549941a6c31a2f525ab4e
David.dionise@detroitlabs.com <David.dionise@detroitlabs.com>
2021-04-28 18:44:50 -0400

    added express types dependency

--- build/index.js
+++ build/index.js
@@ -1,2 +1,9 @@
-console.log("cool");
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var router_1 = require("./router");
+var app = express();
+app.use(express.json());
+app.use(router_1.router);
+app.listen(9999, function () { return console.log("LISTENING"); });
 //# sourceMappingURL=index.js.map
--- src/index.unit.test.ts
+++
@@ -1,5 +0,0 @@
-import { add } from ".";
-
-test("example", () => {
-  expect(add(1, 2)).toBe(3);
-});
